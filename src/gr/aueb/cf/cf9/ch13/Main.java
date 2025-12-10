ec2d10db045a7ae12f48639dceb546091a903447
nicolephmm-source <nikol@MacBookAir.home>
2025-12-10 10:35:04 +0200

    feat: main ch13

--- src/gr/aueb/cf/cf9/ch13/Main.java
+++ src/gr/aueb/cf/cf9/ch13/Main.java
@@ -4,5 +4,10 @@ public class Main {
 
     public static void main(String[] args) {
         HelloUtil.sayHello();
+        OnlyOneCodingFactory onlyOneCodingFactory1 = OnlyOneCodingFactory.getInstance();
+        OnlyOneCodingFactory onlyOneCodingFactory2 = OnlyOneCodingFactory.getInstance();
+
+
+
     }
 }
