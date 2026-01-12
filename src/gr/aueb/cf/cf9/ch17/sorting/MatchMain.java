3fd74d2fcfa269032e3f1c7d6d7abce3ab2cc131
nicolephmm-source <nikol@MacBookAir.home>
2026-01-12 12:01:30 +0200

    feat: add stream API and match

---
+++ src/gr/aueb/cf/cf9/ch17/sorting/MatchMain.java
@@ -0,0 +1,22 @@
+package gr.aueb.cf.cf9.ch17.sorting;
+
+import java.util.List;
+
+public class MatchMain {
+    public static void main(String[] args) {
+        List<String> names = List.of("Alice", "Bob", "Costas");
+        List<Integer> numbers = List.of(1, 2, 3, 4, 5);
+
+        boolean areAllEven = numbers.stream()
+                .allMatch(num -> num % 2 == 0);
+
+        boolean isAliceExists = names.stream()
+                .anyMatch(name -> name.equals("Alice"));
+
+        boolean allAreAlice = names.stream()
+                .allMatch(name -> name.equals("Alice"));
+
+        boolean noneIsAlice = names.stream()
+                .noneMatch(name -> name.equals("Alice"));
+    }
+}
