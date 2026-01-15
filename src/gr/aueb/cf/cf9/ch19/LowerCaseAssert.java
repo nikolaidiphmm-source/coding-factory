a61bf358b71fb91db128e98897f9d6ffa932fc78
nicolephmm-source <nikol@MacBookAir.home>
2026-01-15 14:34:59 +0200

    feat: add zero-length assertions

---
+++ src/gr/aueb/cf/cf9/ch19/LowerCaseAssert.java
@@ -0,0 +1,19 @@
+package gr.aueb.cf.cf9.ch19;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class LowerCaseAssert {
+    public static void main(String[] args) {
+        String s = "HFHGa";   //only lower case, at least 8 char, at least 1 character is lowercase
+
+        Pattern pattern = Pattern.compile("(?=.*?[a-z])^.{8,}$");      //zero-length assertion
+        Matcher matcher = pattern.matcher(s);
+
+        while (matcher.find()) {
+            System.out.println(matcher.group());
+        }
+
+    }
+}
+
